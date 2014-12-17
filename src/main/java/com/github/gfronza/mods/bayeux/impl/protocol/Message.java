1fc23842d3f9589766c58f8c044bc709eec7599d
Germano Fronza <germano@eventials.com>
2014-12-17 08:14:54 -0200

    Message pipe through extensions

--- src/main/java/com/github/gfronza/mods/bayeux/impl/protocol/Message.java
+++ src/main/java/com/github/gfronza/mods/bayeux/impl/protocol/Message.java
@@ -1,10 +1,23 @@
 package com.github.gfronza.mods.bayeux.impl.protocol;
 
+import java.util.List;
+
+import org.vertx.java.core.http.HttpServerRequest;
+
+import com.github.gfronza.mods.bayeux.impl.functions.BayeuxExtension;
+
 /**
  * Represents a message of the Bayeux Protocol.
  * @author Germano
  *
  */
 public class Message {
+
 	// TODO
+	
+	public void pipeThrough(List<BayeuxExtension> extensions, HttpServerRequest request) {
+		for (BayeuxExtension e : extensions) {
+			e.handle(this, request);
+		}
+	}
 }
